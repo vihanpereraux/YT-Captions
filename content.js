00129c12645ae49c6d777c6fd7689dc9a6055d19
vihanpereraux <vihanpereraux@gmail.com>
2025-02-06 04:16:03 +0000

    initial commit

---
+++ content.js
@@ -0,0 +1,37 @@
+// "yt-formatted-string
+
+// Function to change title color
+function changeTitleColor() {
+    const captionWindow = document.getElementsByClassName("caption-window");
+    if (captionWindow) {
+        captionWindow[0].style.textAlign = "center";
+        captionWindow[0].style.left = "50%";
+        captionWindow[0].style.width = "80vw";
+        captionWindow[0].style.marginLeft = "0px";
+        captionWindow[0].style.border = "0px solid red";
+        captionWindow[0].style.transform = "translateX(-50%)";
+    }
+
+    const captionElements = document.getElementsByClassName("ytp-caption-segment");
+    if (captionElements) {
+        for (let i = 0; i < captionElements.length; i++) {
+            captionElements[i].style.fontWeight = "500";
+            captionElements[i].style.background = "rgba(8, 8, 8, 0)";
+            captionElements[i].style.color = "yellow";
+            captionElements[i].style.fontSize = "35px";
+            captionElements[i].style.textTransform = "lowercase";
+        }
+    }
+  }
+  
+  // Observe changes in the page to detect when the title is available
+  const observer = new MutationObserver(() => {
+    changeTitleColor();
+  });
+  
+  // Start observing the body for changes
+  observer.observe(document.body, { childList: true, subtree: true });
+  
+  // Initial check in case the title is already loaded
+  changeTitleColor();
+  
